c4d58798c63de9ef39473151aca038805843c9f7
scottrab <71945998+scottrab@users.noreply.github.com>
2021-02-04 19:20:22 -0500

    Update MechaCarChallenge.Rscript.R

--- MechaCarChallenge.Rscript.R
+++ MechaCarChallenge.Rscript.R
@@ -1,21 +1,22 @@
-Mecha_Car<- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
-
+##read csv file MechaCar_mpg.csv
+M= read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
+##import dplyr 
 Library(dplyr)
-
-model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = Mecha_Car)
-
-summary(model)
-
-Suspension<-read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
-
-summarize_suspension <- Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
-
-Lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
-
-t.test(log10(Suspension$PSI),mu=mean(log10(Suspension$PSI)))
-
-t.test(subset=Suspension$Manufacturing_Lot=="Lot1",(Suspension$PSI),mu=mean(Suspension$PSI))
-
-t.test(subset=Suspension$Manufacturing_Lot=="Lot2",(Suspension$PSI),mu=mean(Suspension$PSI))
-
-t.test(subset=Suspension$Manufacturing_Lot=="Lot3",(Suspension$PSI),mu=mean(Suspension$PSI))
+##linear model with multiple variables predicting mpg
+d= lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = Mecha_Car)
+##summary output for the model
+summary(d)
+##read suspension_coil.csv
+s = read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
+##summarize suspension coil with mean, median and variance
+summarize_s = s ~ summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
+##lot summary for suspension  model
+Lot_summary = s ~ group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
+##run t-test using log
+t.test(log10(s$PSI),mu=mean(log10(s$PSI)))
+##run t-test for lot 1
+t.test(subset=s$Manufacturing_Lot=="Lot1",(s$PSI),mu=mean(s$PSI))
+##run t-test for lot 2
+t.test(subset=s$Manufacturing_Lot=="Lot2",(s$PSI),mu=mean(s$PSI))
+##run t-test for lot 3
+t.test(subset=s$Manufacturing_Lot=="Lot3",(s$PSI),mu=mean(s$PSI))
